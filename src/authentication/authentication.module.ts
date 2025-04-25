af2e1febb46753916444faa4e7943c627b82e59e
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-25 14:46:37 -0300

    feat: integrate JWT module into authentication module and initialize authentication service

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+  })],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
