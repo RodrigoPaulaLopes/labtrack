fd42eeb8641c3466175a2c8f0d390ea10592cb70
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-25 14:49:35 -0300

    feat: implement token creation and validation methods in authentication service

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthenticationService {
 
     
+    constructor(private readonly jwtService: JwtService) {}
+
+
+
+    async createToken() {
+        // return await this.jwtService.signAsync()
+    }
+
+    async validateToken(token: string) {
+        // return await this.jwtService.verifyAsync(token)
+    }
 }
