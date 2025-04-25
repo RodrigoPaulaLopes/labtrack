259c5dab41876975073d49601390ba9b9b46bc4a
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-25 18:26:20 -0300

    feat: add TokenDto class and refactor createToken method to return accessToken

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { AuthDto } from './dto/auth.dto';
 import { User } from 'src/users/entities/user.entity';
+import { TokenDto } from './dto/token.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -20,7 +21,7 @@ export class AuthenticationService {
     }
 
     async createToken(user: User) {
-        return await this.jwtService.signAsync({
+        const token = await this.jwtService.signAsync({
             email: user.email,
             id: user.id,
         }, {
@@ -28,6 +29,7 @@ export class AuthenticationService {
             expiresIn: process.env.JWT_EXPIRES_IN,
             secret: process.env.JWT_SECRET,
         })
+        return { accessToken: token } as TokenDto
     }
 
     async validateToken(token: string) {
---
+++ src/authentication/dto/token.dto.ts
@@ -0,0 +1,8 @@
+
+
+
+export class TokenDto {
+
+    accessToken: string;
+
+}
