890df2b1faf16df42a0f357c9141b66f09ec4461
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-25 15:09:31 -0300

    feat: add validation and properties to CreateUserDto for user registration

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { Not } from "typeorm";
+import { IsNotEmpty, IsOptional, IsStrongPassword } from 'class-validator'
+export class CreateUserDto {
+    
+    @IsNotEmpty()
+    email: string;
+    @IsNotEmpty()
+    @IsStrongPassword({
+        minLength: 8,
+        minUppercase: 1,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+    })
+    password: string;
+    @IsNotEmpty()
+    confirmPassword: string;
+    @IsOptional()
+    firstName?: string;
+    @IsOptional()
+    lastName?: string;
+
+
+}
